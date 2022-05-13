80da010843ed2c3eeca5bc9e54dc8bb819436dd5
Sasa Stevcic <sasa.stevcic@yahoo.com>
2022-05-13 21:49:37 +0200

    Remove responsive

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useLayoutEffect, useState } from 'react';
+import { ReactElement } from 'react';
 import { useCarousel } from '../hooks/useCarousel';
 import {
 	StyledCarousel,
@@ -33,53 +33,8 @@ const DEFAULT_CONFIG = {
 	minVelocity: 100,
 };
 
-const responsive = [
-	{
-		breakpoint: 1024,
-		config: {
-			perView: 2,
-		},
-	},
-	{
-		breakpoint: 768,
-		config: {
-			perView: 1,
-		},
-	},
-];
-
 export const Carousel = ({ children, config, ...props }: CarouselProps): ReactElement => {
 	const mergeConfigs = { ...DEFAULT_CONFIG, ...config };
-	const [responsivePerView, setResponsivePerView] = useState(mergeConfigs.perView);
-
-	useLayoutEffect(() => {
-		responsive
-			.sort((a, b) => b.breakpoint - a.breakpoint)
-			.forEach(({ breakpoint, config: _config }, index) => {
-				const mediaQueryList = window.matchMedia(`(max-width: ${breakpoint}px)`);
-
-				const handleChange = (event: MediaQueryListEvent): void => {
-					if (event.matches) {
-						setResponsivePerView(_config.perView);
-					} else {
-						const previous = responsive[index - 1];
-
-						setResponsivePerView(previous?.config.perView ?? mergeConfigs.perView);
-					}
-				};
-
-				mediaQueryList.addEventListener('change', handleChange);
-
-				if (mediaQueryList.matches) {
-					setResponsivePerView(_config.perView);
-				}
-			});
-	}, [mergeConfigs.perView]);
-
-	if (responsivePerView) {
-		mergeConfigs.perView = responsivePerView;
-	}
-
 	const { perView, showArrows, showDots, ...rest } = mergeConfigs;
 	const {
 		slidesCurrent,
